ce80f6102e1c7f02d25a21bcf93dd313e55b6107
Kosetsue <47840215+Kosetsue@users.noreply.github.com>
2023-08-01 08:30:30 +0800

    Fixing Formatting on new line spaces for text in Dice Battles

--- Ideas/Dice_Battle.py
+++ Ideas/Dice_Battle.py
@@ -7,14 +7,14 @@ Dice_Types = ["D10", "D20", "D100", "D1000"]
 def roll(a, dice_type):
     Roll1 = random.randint(1, a)
     Roll2 = random.randint(1, a)
-    print(f"Rolling the {dice_type}...")
+    print(f"\n\nRolling the {dice_type}...")
 
     if Roll1 > Roll2:
-        print(f"{Name_1} Rolled a {Roll1} and beats {Name_2}'s roll of {Roll2}!")
+        print(f"\n\n{Name_1} Rolled a {Roll1} and beats {Name_2}'s roll of {Roll2}!")
     elif Roll1 < Roll2:
-        print(f"{Name_2} Rolled a {Roll2} and beats {Name_1}'s roll of {Roll1}!")
+        print(f"\n\n{Name_2} Rolled a {Roll2} and beats {Name_1}'s roll of {Roll1}!")
     else:
-        print("Its a tie!")
+        print("\n\nIts a tie!")
 
 
 # ----------------------------------------------------------------------
@@ -23,10 +23,10 @@ if __name__ == "__main__":
         Name_1 = input("What is your name:  ")
         Name_2 = input("What is your opponents name: ")
 
-        print(f"Hello {Name_1} and {Name_2} And Welcome to Dice battles!!!")
-        print("PICK YOUR DICE TYPE!\n", *Dice_Types, sep="\n")
+        print(f"\n\nHello {Name_1} and {Name_2} And Welcome to Dice battles!!!")
+        print("\n\nPICK YOUR DICE TYPE!\n", *Dice_Types, sep="\n")
 
-        Roll = input("What are you rolling: ")
+        Roll = input("\n\nWhat are you rolling: ")
 
         if Roll == "D10":
             roll(10, "D10")
